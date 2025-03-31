860b9c76df1854c8e754f60011ab92aba22ba5a4
Sam <gentalmen28@gmail.com>
2025-04-01 00:18:53 +0100

    Setting up the tools for the Agent along with knowledge base

---
+++ main.py
@@ -0,0 +1,34 @@
+from langgraph.prebuilt import create_react_agent
+from langgraph.checkpoint.memory import MemorySaver
+from langchain_openai import ChatOpenAI
+from langchain_core.tools import tool
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("OPEN_AI_API_KEY")
+base_url = os.getenv("BASE_URL")
+
+## Defining out the tools
+
+KNOWLEDGE_BASE = {
+    "hours":"Our customer support is available from 9 AM to 6 PM, Monday to Friday",
+    "refund":"Refunds are processed within 5-7 business days after approval",
+    "shipping":"We offer standard and express shipping options. standard shipping take 5-7 days, express takes 2-3 days."
+}
+
+@tool
+def search_knowldege_base(query:str):
+    """Searches the basic knowledge base for common customer quereis."""
+    for key, value in KNOWLEDGE_BASE.items():
+        if key in query.lower():
+            return value
+    return "I'm sorry, I could't find relevant information, Please contact support."
+
+@tool
+def contact_support():
+    """Provides contact information for customer support"""
+    return "You can reach our suport team at support@example.com or call us at +44 9876543210."
+
+
